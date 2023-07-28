f2827a41a1ad0d71d591798dbfc513374e311cc5
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-28 15:58:10 +0200

    task 3

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,23 +1,16 @@
 #!/usr/bin/python3
-"""Square class to represent a square"""
+""" Define a A sqaure class """
 
 
 class Square:
-    """
-    Defines a Square and its basic properties
-    >>> square_1 = Square()
-    >>> square_2 = Square(7)
-    """
-
+    """ """
+    
     def __init__(self, size=0) -> None:
-        """
-        Innitialize the size of the square. the size can be specified.
-        If they are not, the size defaults to 0
-        :param size: int size of square ( > 0)
-        """
+        """ check for validations """
+        
         if (type(size) is not int):
-            raise TypeError("size must be an integer")
+            raise TypeError("Size must be an integer")
         elif size < 0:
-            raise ValueError("size must be >= 0")
-
+            raise ValueError("Size must be  >= 0")
+        
         self.__size = size
