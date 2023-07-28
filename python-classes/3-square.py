63aa7fbc35dddda3a6338c89675dff82c36e5525
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-28 16:09:22 +0200

    task 4

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
-""" Define the sqaure class."""
+"""Square class to represent a square"""
 
 
-class Sqaure:
-    """ properties """
-    
+class Square:
+    """
+    Defines a Square and its basic properties
+    """
+
     def __init__(self, size=0) -> None:
-        """ initialize """
-        
+        """
+        Innitialize the size of the square. the size can be specified.
+        If they are not, the size defaults to 0
+        :param size: int size of square ( > 0)
+        """
         if (type(size) is not int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        
+
         self.__size = size
-        
+
     def area(self) -> int:
-        """ return the area of the object"""
+        """
+        Calculates and returns the area of the square
+        :return: the area of the square
+        """
+
         return self.__size ** 2
