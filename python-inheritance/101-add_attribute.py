72b1ddddc8eafdf90c16f1d4557e1b1788b04425
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-28 19:03:41 +0200

    all done

--- python-inheritance/101-add_attribute.py
+++ python-inheritance/101-add_attribute.py
@@ -4,6 +4,7 @@
 
 def add_attribute(obj, att, value):
     """Add a new attribute to an object if possible.
+
     Args:
         obj (any): The object to add an attribute to.
         att (str): The name of the attribute to add to obj.
--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,23 +1,29 @@
 #!/usr/bin/python3
-"""
-more class base
-"""
-
+"""Define a Square class that inherits from Rectangle."""
 
 Rectangle = __import__('9-rectangle').Rectangle
 
 
-"""
-Square class
-"""
-
-
 class Square(Rectangle):
-    """ Square Class """
+    """A class Square that inherits from Rectangle.
+    """
+
     def __init__(self, size):
-        """ size init"""
+        """Initialize a square instance with a given size.
+
+        Args:
+            size (int): The size (side length) of the square.
+        """
+        super().integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
-        super().__init__(self.__size, self.__size)
 
     def __str__(self):
-        return("[Square] " + str(self.__size) + "/" + str(self.__size))
+        """Return an informal string representation of the square.
+        """
+        return f"[Square] {self.__size}/{self.__size}"
+
+    def area(self):
+        """Calculate and return the area of the square.
+        """
+        return self.__size ** 2
