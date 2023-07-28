8084894f0c4a3fff6ce05dba8674aa0b7ee316fa
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-07-28 16:00:13 +0200

    task 3

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-""" Define a A sqaure class """
+""" Sqaure class to represent a sqaure """
 
 
 class Square:
-    """ """
-    
+    """ 
+    Define a square and its basic properties
+    """
+
     def __init__(self, size=0) -> None:
-        """ check for validations """
-        
+        """check for order by
+        ensuring that int is entered
+        """
+
         if (type(size) is not int):
             raise TypeError("Size must be an integer")
         elif size < 0:
-            raise ValueError("Size must be  >= 0")
-        
+            raise ValueError("Size must be >= 0")
+
         self.__size = size
+            
