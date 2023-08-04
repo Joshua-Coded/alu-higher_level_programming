72bd376d25cf4430c86c899ea1cdd823d65a8f29
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-04 15:20:57 +0200

    advanced tasks + s

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# requested
-curl -s  GET -H "X-HolbertonSchool-User-Id: 98" "$1"
+# takes in a URL as an argument, sends a GET request to the URL, and displays the body of the response
+curl -sH "X-School-User-Id: 98" "$1"
