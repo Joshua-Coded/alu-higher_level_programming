173259be80b3837a5143e431de4838f7a450f644
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-04 16:28:27 +0200

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
-# Write a Python script that fetches https://alu-intranet.hbtn.io/status
+"""Python script that fetches https://alu-intranet.hbtn.io/status"""
+
 
 import urllib.request
 
-custom_status = b'Custom status'
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    decoded_html = html.decode('utf-8')
-
-    print("Body response:")
-    print("\t- type: {}".format(type(html)))
-    print("\t- content: {}".format(custom_status))
-    print("\t- utf8 content: {}".format(decoded_html))
+if _name_ == '_main_':
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        content = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf-8")))
