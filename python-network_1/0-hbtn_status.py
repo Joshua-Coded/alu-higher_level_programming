64eb24e3e885caba10f4999f28219f4e3e2efa26
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-04 15:52:56 +0200

    error fixed

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -13,3 +13,16 @@ with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(custom_status))
     print("\t- utf8 content: {}".format(decoded_html))
+
+
+
+
+if __name__ == '__main__':
+    import urllib.request
+
+    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
+        content = res.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode('utf-8')))
