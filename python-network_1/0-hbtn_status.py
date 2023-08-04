9e91323394d6ee87a81d557464c3f030f3ecede8
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-04 15:39:53 +0200

    error fixed

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,7 +5,8 @@ import urllib.request
 
 with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
     html = response.read()
+    decoded_html = html.decode('utf-8')
     print("Body response:")
     print("\t- type: {}".format(type(html)))
-    print("\t- content:\n\t  Custom Status\n{}\n\t  Custom Status".format(html.decode('utf-8')))
-    print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    print("\t- content:\n\t  Custom Status\n{}\n\t  Custom Status".format(decoded_html))
+    print("\t- utf8 content: {}".format(decoded_html))
