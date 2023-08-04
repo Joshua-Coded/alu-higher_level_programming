394ae20f7a67eea71c89624a74b6f70fe93c9332
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-04 16:27:12 +0200

    task 0

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -5,7 +5,7 @@ import urllib.request
 
 custom_status = b'Custom status'
 
-with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
     decoded_html = html.decode('utf-8')
 
@@ -13,16 +13,3 @@ with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as response:
     print("\t- type: {}".format(type(html)))
     print("\t- content: {}".format(custom_status))
     print("\t- utf8 content: {}".format(decoded_html))
-
-
-
-
-if __name__ == '__main__':
-    import urllib.request
-
-    with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
-        content = res.read()
-        print("Body response:")
-        print("\t- type: {}".format(type(content)))
-        print("\t- content: {}".format(content))
-        print("\t- utf8 content: {}".format(content.decode('utf-8')))
