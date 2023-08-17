ccb14f5f9821da100d6b1d805243f2596a7329a0
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-08-17 19:27:50 +0200

    orm

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -7,7 +7,6 @@ from model_city import City
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
-
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
@@ -16,6 +15,4 @@ if __name__ == "__main__":
     session = Session()
     for instance in (session.query(State.name, City.id, City.name)
                      .filter(State.id == City.state_id)):
-        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
-
-
+        print(instance[0] + ': (' + str(instance[1]) + ') ' + instance[2])
