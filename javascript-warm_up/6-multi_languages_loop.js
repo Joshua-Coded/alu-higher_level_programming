a65f1dcf608c6d2cf98c22e64432891f0c10a4b2
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-01 18:05:12 +0200

    task 7 done ☑️

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-let lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < lines.length; i++) {
   console.log(lines[i]);
 }
