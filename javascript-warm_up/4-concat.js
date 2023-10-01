eef54e1dc56c17d19fe4093d7cf7299363f7126a
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-01 16:42:24 +0200

    task 5 done ☑️

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -2,7 +2,7 @@
 if (process.argv[3]) {
   console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[3]);
 } else if (process.argv[2]) {
-  console.log(process.argv[2] + ' ' + ' is ' + ' ' + undefined);
+  console.log(process.argv[2] + ' ' + 'is' + ' ' + undefined);
 } else {
   console.log(process.argv[2] + ' ' + 'is' + ' ' + process.argv[2]);
 }
