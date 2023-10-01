36ff894651e24f11a15ad2e4719094136ae65859
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-01 15:33:06 +0200

    task 2 done ☑️

--- javascript-warm_up/1-multi_languages.js
+++ javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+
+
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
