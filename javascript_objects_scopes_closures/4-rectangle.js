3a2988872a9bb133a834e81367fd1d456852bcea
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-01 22:01:21 +0200

    task 4 ☑️

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -11,7 +11,7 @@ class Rectangle {
     for (let i = 0; i < this.width; i++) {
       let s = '';
       for (let j = 0; j < this.height; j++) {
-        s += 'x';
+        s += 'X';
       }
       console.log(s);
     }
