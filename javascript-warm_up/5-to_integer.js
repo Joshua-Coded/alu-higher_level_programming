01de9d9d6b959edaf8fce84f0137484f3a3598b2
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-01 17:06:44 +0200

    task 6 done ☑️

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,5 +2,5 @@
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
   console.log('Not a number');
 } else {
-  console.log('My number:', + parseInt(process.argv[2]));
+  console.log('My number:', parseInt(process.argv[2]));
 }
