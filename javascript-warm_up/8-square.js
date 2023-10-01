e88d5edb0f49804b400d7aedd497ec742a14efc0
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-10-01 18:34:31 +0200

    task 8 done ☑️

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,7 +2,7 @@
 if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
 } else {
-    const x = Number(process.argv[2]);
+  const x = Number(process.argv[2]);
     let i = 0;
     while (i < x) {
         console.log('X'.repeat(x));
